d2cad7f78b629b6c0fddf670422aac65a5b1302f
Sigurthor Bjorgvinsson <sigurthor12@ru.is>
2017-04-15 12:12:32 -0400

    Added static serve of index.html from dist folder

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 var restify = require("restify");
-var server = restify.createServer();
+var path = require("path");
 var customer = require("./customer.js");
+
+var server = restify.createServer();
 const PORT = 1337;
 
 server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
 customer(server);
+
+server.get("/\/.*/", restify.serveStatic({
+  directory: path.join(__dirname, "..", "dist"),
+  default: "index.html" 
+}));
 
 server.listen(PORT, function(){
   console.log(`listening to port ${PORT}`);
